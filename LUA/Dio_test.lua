1deb66b45ec64a93e2f1dfbef60e722699631960
diogo vieira <diogof-6@hotmail.com>
2019-02-19 22:38:31 +0100

    [test] la fonction ne doit regarder que pour le joueur actif

--- LUA/Dio_test.lua
+++ LUA/Dio_test.lua
@@ -6,9 +6,18 @@
 local civilisationID = GameInfoTypes["CIVILIZATION_TCM_AUSTRIA_HUNGARY"]
 
 function NbVilles(PlayerID)
-	local player = Players[PlayerID]
-	local nbvilles = player:GetNumCities()
-	print(nbvilles.."ville(s) construite(s)");
+	local player = Players[Game.GetActivePlayer()]
+	if( player == Game.GetActivePlayer) then
+		local nbvilles = player:GetNumCities()
+		local isConnected=0
+		print(nbvilles.." ville(s) construite(s)");
+		for city in player:Cities() do
+			if (player:IsCapitalConnectedToCity(city) ) then
+				isConnected = isConnected+1
+			end
+		end
+		print(isConnected.." ville(s) connectée(s)");
+	end
 end
 
 GameEvents.PlayerDoTurn.Add(NbVilles)
