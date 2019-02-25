852ab493132a3a1b115a5af3f694fd07ad018ed4
diogo vieira <diogof-6@hotmail.com>
2019-02-25 23:33:23 +0100

    cleaning of the lua file and cleaning in some methods
    add commentaries to each methods

--- LUA/Base_functions.lua
+++ LUA/Base_functions.lua
@@ -1,103 +1,104 @@
 -- Base_functions
--- Author: Florian
+-- Authors: Florian & Diogo
 -- DateCreated: 2/17/2019 12:51:16 PM
 --------------------------------------------------------------
 local civilizationID = GameInfoTypes["CIVILIZATION_TCM_AUSTRIA_HUNGARY"]
---==========================================================================================================================
+--=============================================================================================
 -- UTILITY FUNCTIONS
---==========================================================================================================================
--- JFD_IsCivilisationActive
---------------------------------------------------------------     
+--=============================================================================================
+
+
+--=============================================================================================
+-- Name:		JFD_IsCivilisationActive
+-- Description:	
+--=============================================================================================     
 function JFD_IsCivilisationActive(civilizationID)
-        for iSlot = 0, GameDefines.MAX_MAJOR_CIVS-1, 1 do
-                local slotStatus = PreGame.GetSlotStatus(iSlot)
-                if (slotStatus == SlotStatus["SS_TAKEN"] or slotStatus == SlotStatus["SS_COMPUTER"]) then
-                        if PreGame.GetCivilization(iSlot) == civilizationID then
-                                return true
-                        end
-                end
+    for iSlot = 0, GameDefines.MAX_MAJOR_CIVS-1, 1 do
+        local slotStatus = PreGame.GetSlotStatus(iSlot)
+        if (slotStatus == SlotStatus["SS_TAKEN"] or slotStatus == SlotStatus["SS_COMPUTER"]) then
+            if PreGame.GetCivilization(iSlot) == civilizationID then
+                return true
+            end
         end
-     
-        return false
+    end 
+    return false
 end
 
---=============================================
---NBConnectionCity
---=============================================
+--=============================================================================================
+-- Name:		NBConnectionCity
+-- Description:	return number of cities connected to capital
+--=============================================================================================
 function NbCityConnected()
     local player = Players[Game.GetActivePlayer()]
 	local nbvilles = player:GetNumCities()
 	local isConnected=0
 	for city in player:Cities() do
-		print(nbvilles.." ville(s) construite(s)")
 		if (player:IsCapitalConnectedToCity(city) and player:GetCivilizationType() == civilizationID ) then
 			isConnected = isConnected+1
 		end
 	end
 	return isConnected
 end
---------------------------------------------------------------
--- GE_Grenzer from TCM's Austria-Hungary
---------------------------------------------------------------
+
+--=============================================================================================
+-- Name:		GE_Grenzer from TCM's Austria-Hungary
+-- Description:	Return the number of units with Grenzschutz promotions
+--=============================================================================================
 function GE_GetNumPromotions(unit)
-        local numPromotions = 0
-			if unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
-				for promotion in GameInfo.UnitPromotions() do
-						if unit:IsHasPromotion(promotion.ID) then
-							numPromotions = numPromotions + 1
-						end
-				end
+	local numPromotions = 0
+	if unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
+		for promotion in GameInfo.UnitPromotions() do
+			if unit:IsHasPromotion(promotion.ID) then
+				numPromotions = numPromotions + 1
 			end
-		return numPromotions
+		end
+	end
+	return numPromotions
 end
 
-local bonusPerPromotion = 1
-local unitGrenzerID = GameInfoTypes["UNIT_TCM_GRENZER"]
-local unitGWIID = GameInfoTypes["UNIT_GREAT_WAR_INFANTRY"]
-local unitInfantryID = GameInfoTypes["UNIT_INFANTRY"]
-local unitMechInfantryID = GameInfoTypes["UNIT_MECHANIZED_INFANTRY"]
-local GEPromotion = GameInfoTypes["PROMOTION_GRENZSCHUTZ"]
-local UnitGun = GameInfoTypes["UNITCOMBAT_GUN"]
-
-
+--=============================================================================================
+-- Name:		GE_Grenzer
+-- Descripion:	Each unit with Grenzschutz's promotion have more 
+--				power per number of promotion (+1 per promotion)
+--=============================================================================================
 function GE_Grenzer(playerID)
     local player = Players[playerID]
+	local bonusPerPromotion = 1
+	local unitGrenzerID = GameInfoTypes["UNIT_TCM_GRENZER"]
+	local unitGWIID = GameInfoTypes["UNIT_GREAT_WAR_INFANTRY"]
+	local unitInfantryID = GameInfoTypes["UNIT_INFANTRY"]
+	local unitMechInfantryID = GameInfoTypes["UNIT_MECHANIZED_INFANTRY"]
+	local GEPromotion = GameInfoTypes["PROMOTION_GRENZSCHUTZ"]
+	local UnitGun = GameInfoTypes["UNITCOMBAT_GUN"]
     local baseCombatStrength
 
     if (player:GetCivilizationType() == civilizationID and player:IsEverAlive()) then
-            for unit in player:Units() do
-                    if unit:GetUnitType() == unitGrenzerID then
-                            baseCombatStrength = GameInfo.Units["UNIT_TCM_GRENZER"].Combat
-                    elseif unit:GetUnitType() == unitGWIID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
-                            baseCombatStrength = GameInfo.Units["UNIT_GREAT_WAR_INFANTRY"].Combat
-                    elseif unit:GetUnitType() == unitInfantryID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
-                            baseCombatStrength = GameInfo.Units["UNIT_INFANTRY"].Combat
-                    elseif unit:GetUnitType() == unitMechInfantryID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
-                            baseCombatStrength = GameInfo.Units["UNIT_MECHANIZED_INFANTRY"].Combat
-                    end
-                    if (unit:GetUnitType() == unitGrenzerID or unit:GetUnitType() == unitGWIID or unit:GetUnitType() == unitInfantryID or unit:GetUnitType() == unitMechInfantryID) and baseCombatStrength < (baseCombatStrength + bonusPerPromotion*GE_GetNumPromotions(unit)) then
-                        unit:SetBaseCombatStrength(baseCombatStrength + bonusPerPromotion*GE_GetNumPromotions(unit))
-                    end
-            end
+        for unit in player:Units() do
+                if unit:GetUnitType() == unitGrenzerID then
+                        baseCombatStrength = GameInfo.Units["UNIT_TCM_GRENZER"].Combat
+                elseif unit:GetUnitType() == unitGWIID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
+                        baseCombatStrength = GameInfo.Units["UNIT_GREAT_WAR_INFANTRY"].Combat
+                elseif unit:GetUnitType() == unitInfantryID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
+                        baseCombatStrength = GameInfo.Units["UNIT_INFANTRY"].Combat
+                elseif unit:GetUnitType() == unitMechInfantryID and unit:IsHasPromotion(GameInfoTypes.PROMOTION_GRENZSCHUTZ) then
+                        baseCombatStrength = GameInfo.Units["UNIT_MECHANIZED_INFANTRY"].Combat
+                end
+                if (unit:GetUnitType() == unitGrenzerID or unit:GetUnitType() == unitGWIID or unit:GetUnitType() == unitInfantryID or unit:GetUnitType() == unitMechInfantryID) and baseCombatStrength < (baseCombatStrength + bonusPerPromotion*GE_GetNumPromotions(unit)) then
+                    unit:SetBaseCombatStrength(baseCombatStrength + bonusPerPromotion*GE_GetNumPromotions(unit))
+                end
         end
-end
-     
-if JFD_IsCivilisationActive(civilizationID) then
-GameEvents.UnitPromoted.Add(GE_Grenzer)
-GameEvents.UnitUpgraded.Add(GE_Grenzer)
-GameEvents.PlayerDoTurn.Add(GE_Grenzer)
+    end
 end
 
---================================================
---UA Scaling Era
---================================================
-
+--=============================================================================================
+-- Name:		UA Scaling Era
+-- Description:	
+--=============================================================================================
 function EraScaling(PlayerID)
-local player = Players[PlayerID]
+	local player = Players[PlayerID]
 	if player:GetCivilizationType() == civilizationID then
 		for city in player:Cities() do
 		if city:IsHasBuilding(GameInfoTypes.BUILDING_DF_CONNECTED) then
-
 				if player:GetCurrentEra() == GameInfoTypes.ERA_FUTURE then
 
 					city:SetBuildingYieldChange(GameInfoTypes.BUILDINGCLASS_DF_CONNECTED, YieldTypes.YIELD_PRODUCTION, 16)
@@ -133,111 +134,115 @@ local player = Players[PlayerID]
 					city:SetBuildingYieldChange(GameInfoTypes.BUILDINGCLASS_DF_CONNECTED, YieldTypes.YIELD_PRODUCTION, 4)
 					city:SetBuildingYieldChange(GameInfoTypes.BUILDINGCLASS_DF_CONNECTED, YieldTypes.YIELD_GOLD, 4)
 					city:SetBuildingYieldChange(GameInfoTypes.BUILDINGCLASS_DF_CONNECTED, YieldTypes.YIELD_FAITH, 2)
-					else
+				end
 			end
-		end
 		end
 	end
 end
-GameEvents.PlayerDoTurn.Add(EraScaling)
---=====================================================
---UAConnectionCities
---=====================================================
+
+--=============================================================================================
+-- Name:		UAConnectionCities
+-- Description:	
+--=============================================================================================
 function UAConnection(PlayerID)
 	local BuildingDummyForConnected = GameInfoTypes.BUILDING_DF_CONNECTED
 	local player = Players[PlayerID]
 
-if player:GetCivilizationType() == civilizationID then
-	for city in player:Cities() do
-		if Player:IsCapitalConnectedToCity(city) and player:IsEverAlive() and not city:IsCapital() then
-		city:SetNumRealBuilding(GameInfoTypes.BUILDING_DF_CONNECTED,1)
+	if player:GetCivilizationType() == civilizationID then
+		for city in player:Cities() do
+			if Player:IsCapitalConnectedToCity(city) and player:IsEverAlive() and not city:IsCapital() then
+				city:SetNumRealBuilding(GameInfoTypes.BUILDING_DF_CONNECTED,1)
+			end
 		end
 	end
 end
-end
-GameEvents.PlayerDoTurn.Add(UAConnection)
---=====================================================
---UAConnectionCapital
---=====================================================
+
+--=============================================================================================
+-- Name:		UAConnectionCapital
+-- Description:	
+--=============================================================================================
 function UAFranzCapital(PlayerID)
 	local BuildingDummyForConnected = GameInfoTypes.BUILDING_DF_CONNECTED
 	local player = Players[PlayerID]
 
-if player:GetCivilizationType() == civilizationID then
-	for city in player:Cities() do
-		if city:IsCapital() then
-		city:SetNumRealBuilding(GameInfoTypes.BUILDING_DF_CONNECTED,1)
+	if player:GetCivilizationType() == civilizationID then
+		for city in player:Cities() do
+			if city:IsCapital() then
+				city:SetNumRealBuilding(GameInfoTypes.BUILDING_DF_CONNECTED,1)
+			end
 		end
 	end
 end
-end
-GameEvents.PlayerCityFounded.Add(UAFranzCapital)
---=====================================================
---UB Kaiserliche Hofbibliothek Connection Capital
---=====================================================
+
+--=============================================================================================
+-- Name:		UB Kaiserliche Hofbibliothek Connection Capital
+-- Description:	
+--=============================================================================================
 function UBKH(PlayerID)
 	local player = Players[PlayerID]
 	local KHBuilding = GameInfoTypes.BUILDING_TCM_CONCERT_HALL
 	local KHBuildingClass = GameInfoTypes.BUILDINGCLASS_NATIONAL_COLLEGE
 
 	if player:GetCivilizationType() == civilizationID then
-	print("ok")
+		print("ok")
 		for city in player:Cities() do
-		print("ok1")
+			print("ok1")
 			if city:IsHasBuilding(KHBuilding) then
-			print("ok2")
-			city:SetBuildingYieldChange(KHBuildingClass, YieldTypes.YIELD_CULTURE, NbCityConnected())
-			--print(NbCityConnected.." on gagne normalement ca")
+				print("ok2")
+				city:SetBuildingYieldChange(KHBuildingClass, YieldTypes.YIELD_CULTURE, NbCityConnected())
 			end
 		end
 	end
 end
-GameEvents.PlayerDoTurn.Add(UBKH)
---======================================================================
---UB Kaiserliche Hofbibliothek_GG_GA
---======================================================================
+
+--=============================================================================================
+-- Name:		UB Kaiserliche Hofbibliothek_GG_GA
+-- Description:	
+--=============================================================================================
 function CheckTrading(PlayerID,CityID)
     local player = Players[PlayerID]
-		for city in player:Cities() do
-			if player:GetCivilizationType() == civilizationID and city:IsHasBuilding(GameInfoTypes.BUILDING_TCM_CONCERT_HALL) then
+	for city in player:Cities() do
+		if player:GetCivilizationType() == civilizationID and city:IsHasBuilding(GameInfoTypes.BUILDING_TCM_CONCERT_HALL) then
 			player:SetNumFreePolicies(1)
 			player:SetNumFreePolicies(0)
 			player:SetHasPolicy(iPolicy, true)
-			end
 		end
+	end
 end
-GameEvents.CityConstructed.Add(CheckTrading)
+
 --=============================================================================================
---KH_Viribus Unitis
+--Name:			ViribusUnitis
+--Description:	Melee and Gun units have a strength bonus per city connected to capital.
+--				Siege units have a ranged combat bonus per city connected to capital.
 --=============================================================================================
 function ViribusUnitis(PlayerID)
+	-- local NBDummy = player:CountNumBuildings(GameInfoTypes.BUILDING_DF_CONNECTED)
     local player = Players[PlayerID]
-    local baseCombatStrength
-	local i=0
-	local NBDummy = player:CountNumBuildings(GameInfoTypes.BUILDING_DF_CONNECTED)
 	local NbConnect = NbCityConnected()
 
     if (player:GetCivilizationType() == civilizationID and player:IsEverAlive()) then
         for unit in player:Units() do
-            if unit:IsHasPromotion(GameInfoTypes.PROMOTION_VIRIBUS_UNITIS) and unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_MELEE or unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_GUN then
-				local sUnit = unit:GetBaseCombatStrength()
+            if unit:IsHasPromotion(GameInfoTypes.PROMOTION_VIRIBUS_UNITIS) and (unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_MELEE or unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_GUN) then
+				local baseCombatStrength = GameInfo.Units[unit:GetUnitType()].Combat;
+				unit:SetBaseCombatStrength(baseCombatStrength + (0.15*NbConnect))
 
-				i = i+1
-
-				local unitType = unit:GetUnitType();
-				
-				local force = GameInfo.Units[unit:GetUnitType()].Combat;
-
-				unit:SetBaseCombatStrength(force + (0.15*(NbCityConnected())))
-
-				elseif unit:IsHasPromotion(GameInfoTypes.PROMOTION_VIRIBUS_UNITIS) and unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_SIEGE then
-						local rUnitType = unit:GetUnitType();
-
-						local rForce = GameInfo.Units[unit:GetUnitType()].RangedCombat;
-
-						unit:SetBaseRangedCombatStrength(rForce + (0.15*(NbCityConnected())))
-				end
+			elseif unit:IsHasPromotion(GameInfoTypes.PROMOTION_VIRIBUS_UNITIS) and unit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_SIEGE then
+				local baseCombatStrength = GameInfo.Units[unit:GetUnitType()].RangedCombat;
+				unit:SetBaseRangedCombatStrength(baseCombatStrength + (0.15*NbConnect))
+			end
 		end
 	end
 end
-GameEvents.PlayerDoTurn.Add(ViribusUnitis)
+
+-- if is a player, events are executed
+if JFD_IsCivilisationActive(civilizationID) then
+	GameEvents.UnitPromoted.Add(GE_Grenzer)
+	GameEvents.UnitUpgraded.Add(GE_Grenzer)
+	GameEvents.PlayerDoTurn.Add(GE_Grenzer)
+	GameEvents.PlayerDoTurn.Add(EraScaling)
+	GameEvents.PlayerDoTurn.Add(UAConnection)
+	GameEvents.PlayerCityFounded.Add(UAFranzCapital)
+	GameEvents.PlayerDoTurn.Add(UBKH)
+	GameEvents.CityConstructed.Add(CheckTrading)
+	GameEvents.PlayerDoTurn.Add(ViribusUnitis)
+end
